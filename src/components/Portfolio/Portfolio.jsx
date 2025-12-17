99b4118ec8924a20e4567690d106d3fc9560c105
Walter Z <wzunigah@gmail.com>
2025-12-16 19:10:34 -0500

    Adding changes to hide construction modal

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,15 +8,13 @@ function Portfolio() {
   return (
     <section className="portfolioContainer" id="portfolio">
       <h2 className="portfolio-heading">Portfolio</h2>
-      {portfolioDict.map(
-        (value, index) =>
-          <Project
-            projectDict={value}
-            firstPortfolio={index === 0}
-            position={index % 2 === 0}
-            key={`portfolio-project-${index}`}
-          />
-      )}
+      {portfolioDict.map((value, index) => (
+        <Project
+          projectDict={value}
+          index={index}
+          key={`portfolio-project-${index}`}
+        />
+      ))}
     </section>
   )
 }
--- src/components/Portfolio/Project.jsx
+++ src/components/Portfolio/Project.jsx
@@ -5,59 +5,37 @@ import { CImage } from "@coreui/react";
 import { CCarouselCaption } from "@coreui/react";
 import "./Project.scss";
 
-function Project({ projectDict, firstPortfolio, position }) {
-  const isMobile = window.innerWidth <= 768;
+function Project({ projectDict, index }) {
+  const isEven = index % 2 === 0;
 
-  const details = (
-    <div className="infoCarousel">
-      <h2>{projectDict.name}</h2>
-      <div className="listInfoProject">
-        <ul>
-          {projectDict.specifications.map((value, index) => (
-            <li className="sublistInfoProject" key={`listInfo-project-${index}`}>
-              {value}
-            </li>
+  return (
+    <div className={`project-card ${isEven ? "project-card--even" : "project-card--odd"}`}>
+      <div className="project-card__content">
+        <h2 className="project-card__title">{projectDict.name}</h2>
+        <ul className="project-card__specs">
+          {projectDict.specifications.map((value, i) => (
+            <li key={`spec-${i}`}>{value}</li>
           ))}
         </ul>
       </div>
-    </div>
-  );
 
-  const carousel = (
-    <div className="carouselContainer">
-      <CCarousel controls indicators dark className="responsiveCarousel">
-        {projectDict.images.map((image, index) => (
-          <CCarouselItem key={`carouselContainer-project-${index}`}>
-            <CImage
-              className="d-block w-100 imageCarousel"
-              src={image.img}
-              alt={`slide ${index + 1}`}
-            />
-            <CCarouselCaption className="d-none d-md-block captionCarousel">
-              <h5>{image.description_1}</h5>
-              <p>{image.description_2}</p>
-            </CCarouselCaption>
-          </CCarouselItem>
-        ))}
-      </CCarousel>
-    </div>
-  );
-
-  return (
-    <div
-      className={`innerBoxPortfolio ${firstPortfolio ? "" : "firstPortfolio"}`}
-    >
-      {isMobile ? (
-        <>
-          {details}
-          {carousel}
-        </>
-      ) : (
-        <>
-          {position ? details : carousel}
-          {position ? carousel : details}
-        </>
-      )}
+      <div className="project-card__gallery">
+        <CCarousel controls indicators dark interval={false}>
+          {projectDict.images.map((image, i) => (
+            <CCarouselItem key={`img-${i}`}>
+              <CImage
+                className="d-block w-100 project-card__image"
+                src={image.img}
+                alt={`Slide ${i + 1}`}
+              />
+              <CCarouselCaption className="d-none d-md-block ">
+                <h5>{image.description_1}</h5>
+                <p>{image.description_2}</p>
+              </CCarouselCaption>
+            </CCarouselItem>
+          ))}
+        </CCarousel>
+      </div>
     </div>
   );
 }
